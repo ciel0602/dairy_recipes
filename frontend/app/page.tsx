a94bab92b9e2106b1eaa687389448b70f1049f96
ciel6622 <>
2026-08-19 17:58:41 +0900

    fix by rubocop

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,13 @@
 import useSWR from 'swr'
 import { fetcher } from '@/app/api/index'
 
-export default function Index() {
+export default function HomePage() {
   const url = 'http://localhost:3000/api/v1/health_check'
   const { data, error } = useSWR(url, fetcher)
 
   if (error) return <div>An error has occurred.</div>
   if (!data) return <div>Loading...</div>
+
 
   return (
     <>
